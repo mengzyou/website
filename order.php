50100accc541ab74b470e8b4bbede3701d6dd2c5
Mengz You <mengz.you@gmail.com>
2012-08-18 12:05:36 +0800

    Add order.php page layout.

---
+++ order.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Order Products</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="js/cufon-yui.js"></script>
+        <script type="text/javascript" src="js/Book_Antiqua_400.font.js"></script>
+        <script type="text/javascript">
+            Cufon.replace ('h1')('h3')('.top_span_48')('.top_span_36')('.top_span_32')('.but1')('.antiqua');
+        </script>
+    </head>
+
+    <body>
+        <div id="container">
+            <header id="root">
+                <div id="header_logo">
+                    <a href="index.html"><img src="images/Baby_header.png" alt="Header Logo" /></a>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="index.html" class="antiqua">Home</a></li>
+                        <li><a href="products.html" class="antiqua">Products</a></li>
+                        <li><a href="aboutus.html" class="antiqua">About us</a></li>
+                        <li class="active"><a href="#" class="antiqua">Order</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <div id="main">
+                <!-- header begins -->
+                <div id="content_header">
+                    <div id="logo">
+                        <a href="index.html"><img src="images/Shunkeda_logo.png" alt="Logo" height="100" /></a>
+                    </div>
+                    <div id="top">
+                        <span style="padding-left: 48px" class="top_span_48 uppercase">Need Help?</span>
+                        <span style="padding-left: 48px;" class="top_span_32">We Know What To Do!</span><br />
+                    </div>
+                </div>
+                <!-- header ends -->
+
+                <div id="content" >
+		    <div id="content_order">
+			<h1>Order</h1>
+        		<form action="order.php" name="order" method="post">
+			    <label>Building</label>
+        		</form>
+		    </div>
+                </div>
+
+                <div id="bottom">
+                    <div class="bottom_box1">
+                        <h3>Related Links</h3>
+                        <ul class="bottom_ul">
+                            <li><a href="#">Link 1</a></li>
+                            <li><a href="#">Link 2</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="bottom_box3">
+                        <div class="ul_img_text">
+                            <h3>Follow Us</h3>
+                            <div class="ul_img_link ul_img_link2"><a href="#">Be a fan on Facebook</a></div>
+                            <div class="ul_img_link ul_img_link4"><a href="#">Follow us on Twitter</a></div>
+                            <div class="ul_img_link ul_img_link5"><a href="#">Follow us on Sina</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="foot_line"></div>
+                <div id="footer">
+                    <p>&copy; 2012. Company Name. Inc. | <a href="#">Privacy Policy</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
